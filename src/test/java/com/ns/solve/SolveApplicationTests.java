6c5a9c4c7d60896d8754c362a809025a887119c6
downfa11 <downfa11@naver.com>
2024-12-07 21:53:18 +0900

    init commit

---
+++ src/test/java/com/ns/solve/SolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns.solve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
